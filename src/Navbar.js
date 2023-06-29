d7dd67ea109fd7478750dd3ef1c335172450f7e9
Ondrej Jedinak <ondrej.jedinak@gmail.com>
2023-06-29 19:39:48 +0200

    navbar rework

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,84 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll"
 import { Link as RedirectLink } from "react-router-dom";
 import logo from "./images/logo-transformed.jpeg"
 import { SocialIcon } from 'react-social-icons'
-import Hamburger from "./Hamburger";
+// import Hamburger from "./Hamburger";
 
 function Navbar(){
+
+    const [active, setActive] = useState(false)
+
+    const toggleActive = () => {
+        setActive(!active)
+    }
+
     return (
-        <nav className="navbar__actions">
+        <nav className="navbar">
             <div className="logoContainer">
                  <RedirectLink to='/'>
                     <img className="logo" src={logo}></img>
                 </RedirectLink>
             </div>
-            <ul style={{float: 'left', marginLeft: 30+'px'}}>
-                
-                <li>
-                    <Link activeClass="active" smooth spy to='about'>
-                        o nás
-                    </Link>
-                </li>
-                <li>
-                    <Link activeClass="active" smooth spy to='program'>
-                        program
-                    </Link>
-                    
-                </li>
-                <li>
-                    <Link activeClass="active" smooth spy to='support'>
-                        podporte nás
-                    </Link>
-                </li>
-                <li>
-                    <Link activeClass="active" smooth spy to='contact'>
-                        kontakty
-                    </Link>
-                </li>
-                <li>
-                    <RedirectLink to="/faq">
-                        faq
-                    </RedirectLink>
-                </li>
+            <ul className={active ? "active nav-menu" : "nav-menu"}>
+                <div style={{float: 'left', marginLeft: 35+'px'}}>
+                    <li className="nav-item">
+                        <Link activeClass="active" smooth spy to='about'>
+                            o nás
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link activeClass="active" smooth spy to='program'>
+                            program
+                        </Link>
+                        
+                    </li>
+                    <li className="nav-item">
+                        <Link activeClass="active" smooth spy to='support'>
+                            podporte nás
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link activeClass="active" smooth spy to='contact'>
+                            kontakty
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <RedirectLink to="/faq">
+                            faq
+                        </RedirectLink>
+                    </li>
+                </div>   
+                <div className="nav-icons">
+                    <ul className={active ? "active nav-menu" : "nav-menu"}>      
+                        <li className="nav-item">
+                            <SocialIcon url="https://www.facebook.com/HONTfestNavrat"  /> 
+                        </li>
+                        <li className="nav-item">
+                            <SocialIcon url="https://www.youtube.com/@spolocenstvosion8512"/>
+                        </li>
+                        <li className="nav-item">
+                            <SocialIcon url="https://www.instagram.com/hontfest/"/>
+                        </li>             
+                    </ul>
+                </div>
             </ul>
 
-            <ul style={{float: 'right'}}>      
-                <li>
-                    <SocialIcon url="https://www.facebook.com/HONTfestNavrat"  /> 
-                </li>
-                <li>
-                    <SocialIcon url="https://www.youtube.com/@spolocenstvosion8512"/>
-                </li>
-                <li>
-                    <SocialIcon url="https://www.instagram.com/hontfest/"/>
-                </li>             
-            </ul>
+            
 
 
-            <div className="mobileNav">
-                    <RedirectLink to='/'>
-                        <img className="logo" src={logo}></img>
-                    </RedirectLink>
-                    <Link activeClass="active" smooth spy to='about'>
-                        o nás
-                    </Link>
-                    <Link activeClass="active" smooth spy to='program'>
-                        program
-                    </Link>
-                    <Link activeClass="active" smooth spy to='support'>
-                        podporte nás
-                    </Link>
-                    <Link activeClass="active" smooth spy to='contact'>
-                        kontakty
-                    </Link>
-                    <RedirectLink to="/faq">
-                        faq
-                    </RedirectLink>
-                    <Hamburger></Hamburger>
-
+            <div className="mobileNav" onClick={()=> toggleActive()}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
             </div>
-            {/* <h2 style={{float: 'inline-end', paddingLeft: 50, paddingBottom: -20}}>Hontfest 2023</h2> */}
         </nav>
     )
 }
