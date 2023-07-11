850d06a26a69d79c8ad5cc7589fd6427b3f7d7ac
Ondrej Jedinak <ondrej.jedinak@gmail.com>
2023-07-11 11:10:19 +0200

    map pin changes

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -5,10 +5,10 @@ import {IoMdCall, IoMdMail, IoMdContact} from 'react-icons/io'
 function Contact(){
     const location = {
         center: {
-            lat: 48.1681454,
-            lng: 18.878047
+            lat: 48.165916315391726,
+            lng: 18.8846644087941,
         },
-        zoom: 10,
+        zoom: 15,
         address: 'Amfiteáter Dudince'
     }
 
