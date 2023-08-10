912315d9e147b64a229c89587982cb2d588a4f46
Ondrej Jedinak <ondrej.jedinak@gmail.com>
2023-08-10 12:09:43 +0200

    smaller footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,17 +4,7 @@ import sponsors from '../src/images/sponzori_2.png'
 export const Footer = () => {
     return (
         <div className="footer-box">
-            {/* <div className="footer-container">
-                <div className="footer-row">
-                    <div className="footer-column"> */}
-                        {/* <p className="footer-heading">Partneri a sponzori</p> */}
-                        <img src={sponsors} className="footer-img" alt="sponzori" />
-                    {/* </div>  */}
-                     {/* <div className="footer-column"> */}
-                            
-                    {/* </div> 
-                </div>           
-            </div> */}
+           <img src={sponsors} className="footer-img" alt="sponzori" />
         </div>
     )
 }
