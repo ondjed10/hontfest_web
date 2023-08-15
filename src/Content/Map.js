7e6d85bf72279659e18753020d12d39b9a7a44ac
Ondrej Jedinak <ondrej.jedinak@gmail.com>
2023-08-15 11:40:29 +0200

    map changes

--- src/Content/Map.js
+++ src/Content/Map.js
@@ -3,7 +3,7 @@ import React from "react";
 function Map({location}){
 
         return (
-            <iframe src="https://maps.google.com/maps?q=48.16572, 18.88157&z=15&output=embed" style={{border: 0+'px'}} width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
+            <iframe src="https://maps.google.com/maps?q=48.16555, 18.88295&z=15&output=embed" style={{border: 0+'px'}} width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
             </iframe>
         )
 }
